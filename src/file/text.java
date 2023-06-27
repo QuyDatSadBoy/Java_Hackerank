3009fd64e6cac81ffe1f6e6ccb2f7c4fd592cf2a
Quy Dat yl09 <dattqyl09@gmail.com>
2023-06-27 10:37:21 +0700

    oke

--- src/file/text.java
+++ src/file/text.java
@@ -1,4 +1,21 @@
 package file;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class text {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("input.txt"));
+            while (sc.hasNext()) {
+                int x = sc.nextInt();
+                System.out.print(x + " ");
+            }
+            System.out.println();
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found !");
+        }
+    }
 }
